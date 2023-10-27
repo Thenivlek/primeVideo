90993f2a3ac7eb545f540f5f34103db6c294d544
Kelvin Viana <kelvin_piantino@hotmail.com>
2023-10-27 17:28:00 -0300

    first commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export const Home = () => {
+  <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
